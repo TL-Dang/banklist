eaf2d05bc5afd5567819a1517ea4c2c6a39249a2
Tommy Dang <ledangt310@live.com>
2022-08-26 16:09:42 -0700

    added container for each movement

--- script.js
+++ script.js
@@ -55,3 +55,25 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayMovements = function (movements) {
+  //setting innerHTML to empty string will empty the containers.
+    containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+        `;
+    //adds new 'movements' to the current balance whether deposit or withdrawal. Use 'afterbegin' bc newest entry will appear at the top of the list. 'beforebegin' will have oldest container always appear on top.
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
