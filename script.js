e3b44ce2a78f2957172a50f47c57c00cec9bab6f
Tommy Dang <ledangt310@live.com>
2022-09-13 16:24:56 -0700

    added loan functionality

--- script.js
+++ script.js
@@ -154,6 +154,22 @@ btnLogin.addEventListener('click', function (e) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    //Add movements
+    currentAccount.movements.push(amount);
+
+    //update UI
+    updateUI(currentAccount);
+  }
+  //clearing the input field
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
